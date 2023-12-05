8d736a7a8d3e3077d7456cb4713935352702fe9c
Tomiecisco <aremujoseph43@gmail.com>
2023-12-05 11:33:29 +0100

    i

---
+++ Day 3/a.py
@@ -0,0 +1,128 @@
+eng_sche = []
+total = 0
+def grab_full_number(cell, row):
+    """Retrieve the full number starting from the specified position with given steps."""
+    number = []
+
+    i = cell
+
+    # Check left side
+    while i >= 0:
+        if str(eng_sche[row][i]).isdigit():
+            number.insert(0, eng_sche[row][i])
+            i -= 1
+            continue
+
+        break
+
+    z = cell + 1
+    # Check right side
+    while z < len(eng_sche[row]):
+        if str(eng_sche[row][z]).isdigit():
+            number.append(eng_sche[row][z])
+            z += 1
+            continue
+
+        break
+
+    return (int(''.join(number)))
+def searcher():
+    numbers = []
+    for i, j in enumerate(eng_sche):
+        for k, l in enumerate(j):
+            if l != '.' and not l.isdigit():
+                # print("left",find_adj_l(i, k))
+                # print("right",find_adj_r(i, k))
+                # print("top",find_adj_u(i, k))
+                # print("top left",findadj_ul(i, k))
+                # print("top right",findadj_ur(i, k))
+                # print("buttom",find_adj_b(i, k))
+                # print("buttom left",findadj_bl(i, k))
+                # print("buttom right",findadj_br(i, k))
+                if find_adj_u(i,k).isdigit():
+                    numbers.append(grab_full_number(k, i - 1))   
+                    
+                elif findadj_ul(i,k).isdigit() or findadj_ur(i,k).isdigit():  
+                    if findadj_ul(i,k).isdigit():
+                        numbers.append(grab_full_number(k - 1, i - 1))
+
+                    if findadj_ur(i,k).isdigit():
+                       numbers.append(grab_full_number(k + 1, i - 1))
+
+                if find_adj_b(i,k).isdigit():
+                    numbers.append(grab_full_number(k, i + 1))
+                elif findadj_bl(i,k).isdigit() or findadj_br(i,k).isdigit():  
+                    if findadj_bl(i,k).isdigit():
+                        numbers.append(grab_full_number(k - 1, i + 1))
+
+                    if findadj_br(i,k).isdigit():
+                        numbers.append(grab_full_number(k + 1, i + 1))
+                if find_adj_l(i,k).isdigit():
+                    numbers.append(grab_full_number(k-1, i))
+                if find_adj_r(i,k).isdigit():
+                    numbers.append(grab_full_number(k+1, i))
+   
+        
+    return numbers
+                
+
+def find_adj_l(i, k):
+    if k != 0:
+        return eng_sche[i][k-1]
+    else:
+        return '.'   
+
+def find_adj_r(i, k):
+    if k != len(eng_sche[i]) -1:
+        return  eng_sche[i][k+1]
+    else:
+        return '.'   
+    
+def find_adj_b(i, k):
+    if i != len(eng_sche)-1:
+        return  eng_sche[i+1][k]
+    else:
+        return '.'
+    
+def find_adj_u(i, k):
+    if i != 0:
+        return eng_sche[i-1][k]
+    else:
+        return '.'   
+    
+def findadj_ul(i,k):
+    if i != 0 and k !=0:
+        return eng_sche[i-1][k-1]
+    else:
+        return '.'
+    
+def findadj_ur(i,k):
+    if i != 0 and k != len(eng_sche[i]) -1:
+        return eng_sche[i-1][k+1]
+    else:
+        return '.'
+
+def findadj_bl(i,k):
+    if i != len(eng_sche)-1 and k!=0:
+        return  eng_sche[i+1][k-1]
+    else:
+        return '.'
+
+def findadj_br(i,k):
+   if i != len(eng_sche)-1 and len(eng_sche[i]) -1:
+        return  eng_sche[i+1][k+1]
+   else:
+        return '.'
+
+
+
+
+def main():
+    with open("Day 3\input.txt", 'r') as reader:
+       for line in reader.readlines():
+           eng_sche.append(list(line.strip()))
+    print(sum(searcher()))
+       
+
+if __name__ == "__main__":
+    main()
---
+++ Day 3/a1.py
@@ -0,0 +1,112 @@
+DATA_MATRIX = []
+
+
+def setup():
+    """Read input file and populate the data matrix."""
+    with open("Day 3\input.txt", "r") as reader:
+        for line in reader.readlines():
+            DATA_MATRIX.append(list(line.strip()))
+
+
+def grab_full_number(cell, row):
+    """Retrieve the full number starting from the specified position with given steps."""
+    number = []
+
+    i = cell
+
+    # Check left side
+    while i >= 0:
+        if str(DATA_MATRIX[row][i]).isdigit():
+            number.insert(0, DATA_MATRIX[row][i])
+            i -= 1
+            continue
+
+        break
+
+    z = cell + 1
+    # Check right side
+    while z < len(DATA_MATRIX[row]):
+        if str(DATA_MATRIX[row][z]).isdigit():
+            number.append(DATA_MATRIX[row][z])
+            z += 1
+            continue
+
+        break
+
+    return (int(''.join(number)))
+
+
+def find_adjacents(cell, row):
+    """Find adjacent numbers for the current position."""
+    numbers = []
+
+    left = DATA_MATRIX[row][cell-1] if cell != 0 else None
+
+    if str(left).isdigit():
+        numbers.append(grab_full_number(cell-1, row))
+
+    right = DATA_MATRIX[row][cell +
+                             1] if cell < (len(DATA_MATRIX[row]) - 1) else None
+
+    if str(right).isdigit():
+        numbers.append(grab_full_number(cell+1, row))
+
+    top = DATA_MATRIX[row - 1][cell] if row != 0 else None
+
+    if str(top).isdigit():
+        numbers.append(grab_full_number(cell, row - 1))
+
+    else:
+        top_left = DATA_MATRIX[row - 1][cell -
+                                        1] if row != 0 and cell != 0 else None
+
+        if str(top_left).isdigit():
+            numbers.append(grab_full_number(cell - 1, row - 1))
+
+        top_right = DATA_MATRIX[row - 1][cell +
+                                         1] if row != 0 and cell < (len(DATA_MATRIX[row]) - 1) else None
+
+        if str(top_right).isdigit():
+            numbers.append(grab_full_number(cell + 1, row - 1))
+
+    bottom = DATA_MATRIX[row +
+                         1][cell] if row < (len(DATA_MATRIX) - 1) else None
+
+    if str(bottom).isdigit():
+        numbers.append(grab_full_number(cell, row + 1))
+
+    else:
+        bottom_left = DATA_MATRIX[row + 1][cell -
+                                           1] if cell != 0 and row < (len(DATA_MATRIX) - 1) else None
+
+        if str(bottom_left).isdigit():
+            numbers.append(grab_full_number(cell - 1, row + 1))
+
+        bottom_right = DATA_MATRIX[row + 1][cell + 1] if row < (
+            len(DATA_MATRIX) - 1) and cell < (len(DATA_MATRIX[row]) - 1) else None
+
+        if str(bottom_right).isdigit():
+            numbers.append(grab_full_number(cell + 1, row + 1))
+
+    return numbers
+
+
+def search_for_chars():
+    """Search for  characters and calculate the sum of adjacent numbers."""
+    total = 0
+    for data_index, data in enumerate(DATA_MATRIX):
+        for item_index, item in enumerate(data):
+            if not item.isdigit() and item != '.':
+                total += sum(find_adjacents(item_index, data_index))
+
+    print(total)
+
+
+def main():
+    """Main entry point."""
+    setup()
+    search_for_chars()
+
+
+if __name__ == "__main__":
+    main()
